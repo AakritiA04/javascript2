314bf27f3f065d8b87fc280f80a3f76a1b99569c
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-05 01:00:37 +0530

    new

--- 04_primitive.js
+++ 04_primitive.js
@@ -1,6 +1,6 @@
 //nn bb ss u
 let a= null;
-let b=365;
+let b=365; // numer
 let c= true; //cana lso be false
 let d= BigInt("567");
 let e="Aakriti";//String hai
@@ -8,3 +8,12 @@ let f= Symbol("I am a nice symbol")
 let g=undefined
 console.log(a,b,c,d,f,g)
 console.log(typeof d); //getting th type of datatype it is 
+
+//objects for js
+const item = {
+    "harry":true,
+    "Aakriti": false,
+    "Aarav": 67,
+    "Bruh": undefined 
+}
+console.log(item["harry"])

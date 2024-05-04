bdc96213cba544efb74cb38d9d4ada6cee6be942
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-04 23:03:52 +0530

    new

--- 03_scopes.js
+++ 03_scopes.js
@@ -2,7 +2,7 @@ console.log("javascripy tutorial var and let and const diff");
 let b = "harry";
 const author = "harry";
 b = 4;
-let c = null;
+let c = null; 
 let d = undefined;
 {
     let b= 'this'

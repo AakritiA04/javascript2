bdddeed711fe90b6cfa2aa68f6a81bf3f8bc8fe3
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-07 01:33:47 +0530

    new

---
+++ 05_opertaors.js
@@ -0,0 +1,28 @@
+console.log("operators in js")
+//aithematic operators 
+let a=45
+let b=4;
+
+console.log("a+b =",a+b)
+console.log("a-b =",a-b)
+console.log("a*b =",a*b)
+console.log("a/b =",a/b)//by didvifing js will show the true value of the operations
+console.log("a**b =",a**b)//this is exponential
+console.log("a%b =",a%b)
+console.log("a++ =",a++)
+console.log("a-- =",a--)
+console.log(a)
+
+
+//assignment operator
+let c=8
+console.log(c)
+//now i woudl like to increment a by 5 digots
+c+=5 //same as c=c+5
+console.log(c)
+
+
+//comaprison operators
+let com1=6;
+let com2=7;
+console.log("com1==com2 is", com1==com2) 

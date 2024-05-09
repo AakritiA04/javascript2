e6f2bc1aae356e923365a625045fdb8845b5cb65
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-09 23:18:23 +0530

    new

---
+++ loops.js
@@ -0,0 +1,8 @@
+//program to add sum of n natural number
+const prompt=require("prompt-sync")({signint:true});
+let sum=0
+let n = prompt("enter the number")
+n= Number.parseInt(n);
+for(let i =0;i<n;i++){
+    console.log(i)
+} 

586da4d64e3ae82ea21acdbb3216b1e64d80e0d3
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-11 23:49:49 +0530

    new

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 //program to add sum of n natural number
 const prompt=require("prompt-sync")({signint:true});
 let sum=0
-let n = prompt("enter the number")
+let n = prompt("enter the number");
 n= Number.parseInt(n);
 for(let i =0;i<n;i++){
     console.log(i+1)
-    sum+=i
+    sum+=i;
 } 
-console.log("Sum of first"+n+"natural numbers")
+console.log("Sum of all the first"+n+"natural numbers")

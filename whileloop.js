e1bc43937fc568643af800e64f3221edc58d2a1b
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-15 00:29:00 +0530

    whileloop

--- whileloop.js
+++ whileloop.js
@@ -1,9 +1,19 @@
+// const prmopt = require("prompt-sync")({singint:true});
+// let n= prompt("enter the value of n")
+// n=Number.parseInt(n);
+// let i=0;
+// while(i<=n){
+//     console.log(i)
+//     i++;
+
+// }
 const prmopt = require("prompt-sync")({singint:true});
 let n= prompt("enter the value of n")
 n=Number.parseInt(n);
 let i=0;
-while(i<n){
-    console.log(n)
+do{
+    console.log(i)
     i++;
 
-}
+}while(i<=n)
+

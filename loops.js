d734e4c92f05d4ae99de6736cc1d5173ad204151
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-14 23:03:23 +0530

    forin loop

--- loops.js
+++ loops.js
@@ -1,11 +1,25 @@
 //program to add sum of n natural number
 // const prompt=require("prompt-sync")({signint:true});
-let sum=0
-let n = prompt("enter the number");
-n= Number.parseInt(n);
-for(let i=0;i<n;i++){
-    // console.log(i+1)
-    sum+=(i+1);
-} 
-console.log("Sum of all the first"+n+"natural numbers" + sum)
+// let sum=0
+// let n = prompt("enter the number");
+// n= Number.parseInt(n);
+// for(let i=0;i<n;i++){
+//     // console.log(i+1)
+//     sum+=(i+1);
+// } 
+// console.log("Sum of all the first"+n+"natural numbers" + sum)
 
+
+let obj ={
+    harry :90,
+    aakriti:99,
+    shubh:77,
+    aarav:89,
+    ritika:80,
+    bye:44,
+    shivalika:22
+}
+
+for (let a in obj){
+    console.log("Marks of "+a +" are " + obj[a])
+}

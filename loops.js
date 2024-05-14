85f55f2b50678a1ba87b07698bbff577d2d22984
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-14 22:57:58 +0530

    new

--- loops.js
+++ loops.js
@@ -3,10 +3,9 @@
 let sum=0
 let n = prompt("enter the number");
 n= Number.parseInt(n);
-for(let i =0;i<n;i++){
-    console.log(i+1)
-    sum+=i;
+for(let i=0;i<n;i++){
+    // console.log(i+1)
+    sum+=(i+1);
 } 
-console.log("Sum of all the first"+n+"natural numbers")
+console.log("Sum of all the first"+n+"natural numbers" + sum)
 
-/

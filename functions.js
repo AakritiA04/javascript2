384cee16068ee2220fa1a3ab71a1a1533a3d7db7
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-17 23:56:27 +0530

    practice

--- functions.js
+++ functions.js
@@ -11,6 +11,7 @@ const sum=(p,q)=>{
 let a = 1;
 let b = 2;
 let c = 3;
+let d= 20;
 
 console.log("one plus Avg of a and b is ", onePlusAvg(a,b))
 console.log("one plus Avg of b and c is ", onePlusAvg(b,c))

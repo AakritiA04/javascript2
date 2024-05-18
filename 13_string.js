fa7bbd39cbe8adead2f491e9b8bc62820d6ff226
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-18 14:22:42 +0530

    strings

---
+++ 13_string.js
@@ -0,0 +1,18 @@
+let name ='Aakriti'
+console.log(name.length)
+console.log(name)
+console.log(name[1])
+let friend="Aarav"
+console.log(friend.length)
+
+//template literal
+let boy1= "pramod"
+let boy2="nikhil"
+
+//nikhil is a friend sof pramod
+let sentence=`${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+//escape sequence character
+let fruit='Bana\'na'
+console.log(fruit.length)

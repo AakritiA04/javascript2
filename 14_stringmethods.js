c1846bb2005ad9046dbdcdd940d472fc0ab527ff
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-19 01:00:26 +0530

    stringsmethods

---
+++ 14_stringmethods.js
@@ -0,0 +1,22 @@
+//length
+let name1 = "Aakriti Bhai"
+console.log(name1.length)
+let string1 = "hello to javavscript world and this is my new program"
+console.log(string1.toUpperCase())
+console.log(name1.slice(2,4))
+console.log(name1.slice(2));
+let newName = name1.replace("Bhai","Arora")
+console.log(newName)
+let friend="BRo"
+console.log(name1.concat(" is a friend of ", friend))
+
+// getting rid of spaces
+let string3 = "     mena    "
+console.log(string3.trim())
+
+//use a for loop to print a stirng\
+let name2="Hello brother"
+for(let i=0;i<name2.length;i++){
+    console.log(name2[i])
+}
+

41d6672d6022c79f0cd55fc67c435af34771531f
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-19 22:56:36 +0530

    practice strings

--- practiceset4.js
+++ practiceset4.js
@@ -20,3 +20,24 @@ console.log(str1.startsWith('Sat', 3));
 console.log(str1.endsWith('best!'));
 //expected outcome false
 
+
+//problem 3
+//to lower case
+console.log(str1.toLowerCase());
+
+//problem 4
+//extract the amount out of the following
+let str2 = "Please give me Rs 1000"
+let amount = Number.parseInt(str2.slice("Please give me Rs ".length))
+console.log(amount)
+
+console.log(typeof amount)
+
+//problem 5
+let friend = "deepika"
+friend[3]='R'
+console.log(friend)
+
+//does not change as string is immutablle 
+
+

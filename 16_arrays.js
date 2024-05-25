b7c839cabb61e3465d6395a0fab88cd7e0a4db0d
AakritiA04 <aroraaakriti04@gmail.com>
2024-05-25 23:29:25 +0530

    arrays

---
+++ 16_arrays.js
@@ -0,0 +1,14 @@
+let marks_class12 =[91, 82, 63, 94,"not present"]
+console.log(marks_class12)
+console.log(marks_class12[0])
+console.log(marks_class12[1])
+console.log(marks_class12[2])
+console.log(marks_class12[3])
+console.log(marks_class12[4])
+console.log(marks_class12.length )
+//addin g the new value the array
+marks_class12[5]=89
+console.log(marks_class12[5])
+//changing the value of  an array
+marks_class12[0]=99
+console.log(marks_class12[0])

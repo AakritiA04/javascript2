637c714c7c53cc63cbafe734e82f91bf3144640b
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-04 11:21:45 +0530

    arrays methods

---
+++ 17_arraymethods.js
@@ -0,0 +1,12 @@
+//Aaary mathods 
+let num = [1,2,3,44,5]
+let b = num.toString();
+console.log(b)
+let c = num.join("-");
+console.log(c,typeof c)
+// num.pop();
+// console.log(num)
+let r = num.shift()
+console.log(r,num )
+let s = num.unshift(66)
+console.log(s, num)

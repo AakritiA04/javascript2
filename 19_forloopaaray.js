fbf3a5f18d5210d96abc55117ad04fcc3539fd34
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-04 23:53:37 +0530

    arrays methods

---
+++ 19_forloopaaray.js
@@ -0,0 +1,18 @@
+let num = [3,54,1,2,4]
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i])
+// }
+
+//foreach method
+num.forEach((element)=>{
+    console.log(element*element)
+})
+//Array.from ka use
+let name = "Aakriti"
+let arr =Array.from(name)
+console.log(arr)
+
+//for...of
+for(let i of num){
+    console.log(i)
+ }

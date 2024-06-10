d58105987029e9cf13617c8d4975b792d2b674cf
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-10 23:04:03 +0530

    arrays methods

---
+++ 21_practiceset5.js
@@ -0,0 +1,36 @@
+//practice of arrays and its methods
+//ques1 
+//create an array of numbers and take input to add numbers tpthe arays
+const prompt = require("prompt-sync")({ signint: true });
+// let arr =[1,2,3,4,5,6,7,83]
+// let a= prompt("Enter a number")
+// a = Number.parseInt(a)
+// arr.push(a)
+// console.log(arr)
+
+
+//practice problem 2
+let arr = [1, 2, 3, 4, 5, 6, 7, 83]
+// let a;
+// do {
+
+//  a = prompt("Enter a number")
+//     a = Number.parseInt(a)
+//     arr.push(a)
+//     console.log(arr)
+// } while (a != 0)
+// console.log(arr)
+
+//filter for numbers divisble by 10 in the given array
+let arr2=[1,10,20,34,100,22,45,30,60,44,56,67]
+function Divisible10(x){
+    return x % 10===0;
+}
+let n = arr2.filter(Divisible10)
+console.log(n)
+
+
+
+
+
+

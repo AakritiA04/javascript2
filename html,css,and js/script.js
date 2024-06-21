463cf1bb9a3b005a011b40c2daa32cfba7a01835
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-22 01:36:37 +0530

    html, css and js together in browser

--- html,css,and js/script.js
+++ html,css,and js/script.js
@@ -1,1 +1,7 @@
-alert("hello")
+console.log(console) //listes all the functions of console
+console.error("hey this is an eror")//red mark me warning
+console.info("hey this is an info")//provides info but is similar to log
+console.assert(5>55)//gives a red warning if the given assertion or condiotion isa false
+console.table()//creates atable out of objects provided
+console.clear()//clears the console window 
+console.warn()//gives warning to the window user

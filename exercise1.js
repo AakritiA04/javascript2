9993bb1900beca913931bb2e48c7596dfce691ad
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-24 23:41:51 +0530

    js exercise

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+//Exercuse ine guess the number
+let number = 0;
+let chance = 0;
+// keep asking for the number untill the correct number is guessed 
+// increment the chance variable 
+// eventually after the game is over displate the (100- number of chnces) and the actual number

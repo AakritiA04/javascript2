470bfa0e1acce3266d216482c2c8ea95db7aeb06
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-26 23:57:02 +0530

    js exer

---
+++ 25_promptconfimetc.js
@@ -0,0 +1,3 @@
+//how user can interact eith user in the browser 
+//alert to give alert to the user
+//confirm to use the confirmation for eg deletion of something and getting confirmation from the user

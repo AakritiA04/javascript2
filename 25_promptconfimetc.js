d933dab394e1f8a52575c36675d290bd569df739
AakritiA04 <aroraaakriti04@gmail.com>
2024-06-27 00:37:45 +0530

    js exer

--- 25_promptconfimetc.js
+++ 25_promptconfimetc.js
@@ -1,3 +1,17 @@
 //how user can interact eith user in the browser 
 //alert to give alert to the user
 //confirm to use the confirmation for eg deletion of something and getting confirmation from the user
+
+
+alert("eneter the value of a!")
+let a = prompt("enter the value of a:")
+a = Number.parseInt(a)
+alert("the value of a of type " + (typeof a))
+let write = confirm("do you want to write it to the page")
+if(write){
+    document.write(a)
+}
+else{
+    document.write("please allow me to write")
+}
+document.write(a)

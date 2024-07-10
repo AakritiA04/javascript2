d4e8744e82fd7ffa49a0b3ae2f1ec33f8d31b875
AakritiA04 <aroraaakriti04@gmail.com>
2024-07-10 23:48:32 +0530

    random generation

--- random.generation.js
+++ random.generation.js
@@ -44,4 +44,17 @@ console.log(randomColor())
 
 //RANDOM PASSWORD GNERATEION
 
+function randomPassword(length){
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    let result ='';
+    for(let i=0; i <length;i ++){
+        result += characters.charAt(Math.floor(Math.random()* characters.length));
+
+    }
+    return result;
+}
+console.log(randomPassword(12));
+
+
+
 

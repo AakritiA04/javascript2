b210ab8c722ac82be9f3a36b645e594d448dd5aa
AakritiA04 <aroraaakriti04@gmail.com>
2024-07-27 20:31:19 +0530

    child accessing

---
+++ html,css,and js/script2.js
@@ -0,0 +1,5 @@
+//accesing firat and second and last child nodes
+
+console.log(document.body.firstChild)//fisrt child is tect nodes
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)

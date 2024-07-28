19a674ff10d4cb15777109d91d3d0c59bf6213b4
AakritiA04 <aroraaakriti04@gmail.com>
2024-07-28 23:36:36 +0530

    child accessing

--- html,css,and js/script2.js
+++ html,css,and js/script2.js
@@ -1,5 +1,10 @@
 //accesing firat and second and last child nodes
 
 console.log(document.body.firstChild)//fisrt child is tect nodes
-console.log(document.body.lastChild)
-console.log(document.body.childNodes)
+console.log(document.body.lastChild)//acccesing last child
+console.log(document.body.childNodes)//accessing all child nodes
+
+let arr= Array.from(document.body.chileNodes)
+
+//sibling and their parents 
+//<html> is the parent of <body and <head> tag

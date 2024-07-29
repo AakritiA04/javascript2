37dfc9de72b22772d435730d979d51b409b1186c
AakritiA04 <aroraaakriti04@gmail.com>
2024-07-29 22:25:57 +0530

    element only navigation

--- html,css,and js/script3.js
+++ html,css,and js/script3.js
@@ -1,4 +1,4 @@
 //element only navigation
 let b= document.body
 console.log("FIrst child of b is:",b.firstChild);
-console.log("FIrst child of b is:",b.firstELementChild);
+console.log("FIrst Element child of b is:",b.firstELementChild);

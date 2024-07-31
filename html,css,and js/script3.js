793aff02cba2b02a2541aaae8ca2af25094e728b
AakritiA04 <aroraaakriti04@gmail.com>
2024-07-31 16:13:59 +0530

    element only navigation

--- html,css,and js/script3.js
+++ html,css,and js/script3.js
@@ -1,5 +1,8 @@
 //element only navigation
+const changeBidyRed = ( )=> {
+    document.body.firstElementChild.style.background="red"
+}
 let b= document.body
 console.log("FIrst child of b is:",b.firstChild);
-console.log("FIrst Element child of b is:",b.firstELementChild);
-console.log()
+console.log("FIrst Element child of b is:",b.firstElementChild);
+

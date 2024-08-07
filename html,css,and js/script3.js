48adc1b4feebf36979ec2925b4781a91ccae2d23
AakritiA04 <aroraaakriti04@gmail.com>
2024-08-07 20:43:00 +0530

    table based navigation

--- html,css,and js/script3.js
+++ html,css,and js/script3.js
@@ -1,8 +1,14 @@
-//element only navigation
-const changeBgRed = ( )=> {
-    document.body.firstElementChild.style.background="red"
-}
-let b= document.body
-console.log("FIrst child of b is:",b.firstChild);
-console.log("FIrst Element child of b is:",b.firstElementChild);
+// //element only navigation
+// const changeBgRed = ( )=> {
+//     document.body.firstElementChild.style.background="red"
+// }
+// let b= document.body
+// console.log("FIrst child of b is:",b.firstChild);
+// console.log("FIrst Element child of b is:",b.firstElementChild);
 
+
+//table based navigation 
+
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t);
+console.log(t.rows)

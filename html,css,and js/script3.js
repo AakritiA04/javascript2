1c4fc3285d43fff433071f2703cbd11fcdde7eb4
AakritiA04 <aroraaakriti04@gmail.com>
2024-08-09 16:17:56 +0530

    table based navigation

--- html,css,and js/script3.js
+++ html,css,and js/script3.js
@@ -12,4 +12,6 @@
 let t = document.body.firstElementChild.firstElementChild;
 console.log(t);
 console.log(t.rows)
-console.log()
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)

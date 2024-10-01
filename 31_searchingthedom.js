1add1af3d3939829ca42b14cdfb85487a72eb754
AakritiA04 <aroraaakriti04@gmail.com>
2024-10-01 23:53:48 +0530

    searching the dom

---
+++ 31_searchingthedom.js
@@ -0,0 +1,13 @@
+//change the color of the elements 
+
+let ctitle=document.getElementById("firstcardtitle");
+ctitle.style.color="blue";
+
+let ctitles=document.querySelectorAll(".card-title")
+    ctitles[0].style.color="blue";
+    ctitles[1].style.color="green";
+    ctitles[1].style.color="yellow";
+
+    console.log(ctitles);
+    document.queryS
+

209cfed1abc95dc71960af030e038b61ac56987a
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-15 01:16:58 +0530

    practice

---
+++ html,css,and js/practiceq2.js
@@ -0,0 +1,7 @@
+let gameNum=25;
+let userNum= prompt("enter your number");
+
+while(gameNum!=userNum){
+   userNum=prompt("enter again as u entered a wrog num"); 
+}
+console.log("congo you entered the right number");

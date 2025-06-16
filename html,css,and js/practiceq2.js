db4cf5b6ab924b1a6cbe767d5dfedfdcf77de9a9
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-16 18:17:12 +0530

    practice

--- html,css,and js/practiceq2.js
+++ html,css,and js/practiceq2.js
@@ -1,10 +1,18 @@
-let gameNum=25;
-let userNum= prompt("enter your number");
+// let gameNum=25;
+// let userNum= prompt("enter your number");
 
-while(gameNum!=userNum){
-   userNum=prompt("enter again as u entered a wrog num"); 
-}
-console.log("congo you entered the right number");
+// while(gameNum!=userNum){
+//    userNum=prompt("enter again as u entered a wrog num"); 
+// }
+// console.log("congo you entered the right number");
 
-//template literals: special string..but hai string hi tbvhf
-let sentence=`hello this is template literal`;
+// //template literals: special string..but hai string hi tbvhf
+// let sentence=`hello this is template literal`;
+
+//practice question after loops
+// Prompt the user to enter their full name . generate a username fot them basd on input . Start username with @ , followed by their full name and ending with the fullname length
+
+let fullname= prompt("enter your name");
+let lengthing = fullname.length;
+
+console.log(`The username of the person is @${fullname}${lengthing}`)

f9c3975df97a7cbd3576e23601e207afcc8b911c
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-20 23:25:51 +0530

    new

--- html,css,and js/practiceq2.js
+++ html,css,and js/practiceq2.js
@@ -35,4 +35,11 @@
 // for a given array with prices of 5 items . all items have an offer od 10% off on them.
 // change the array to store final offer after applyunh it .a
 
-
+let items=[250,645,300,900,50];
+let i=0;
+for(let val of items){
+    let offer = val/10;
+    items[i]=items[i]-offer;
+    console.log(`value after offer = ${items[i]}`);
+    i++;
+}

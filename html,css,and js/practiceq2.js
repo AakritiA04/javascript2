3c4e13b9db8bac21d7a7bff3bcdeca5b0e067542
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-21 23:35:16 +0530

    new

--- html,css,and js/practiceq2.js
+++ html,css,and js/practiceq2.js
@@ -36,10 +36,18 @@
 // change the array to store final offer after applyunh it .a
 
 let items=[250,645,300,900,50];
-let i=0;
-for(let val of items){
-    let offer = val/10;
+// let i=0;
+// for(let val of items){
+//     let offer = val/10;
+//     items[i]=items[i]-offer;
+//     console.log(`value after offer = ${items[i]}`);
+//     i++;
+// }
+
+
+//using for loop
+for(let i=0; i<items.length;i++){
+    let offer = items[i]/10;
     items[i]=items[i]-offer;
-    console.log(`value after offer = ${items[i]}`);
-    i++;
+    console.log(`value after the offer =${items[i]}`);
 }

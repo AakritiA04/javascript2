ac22bfdd9f567f90368928185fcbfd8745fc73b0
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-25 01:09:27 +0530

    functions

---
+++ html,css,and js/functions.js
@@ -0,0 +1,12 @@
+//firsts lets do arrays
+let companies=["Bloomberg","Microsoft","Google", "Uber", "IBM", " Netflix"];
+console.log(companies)
+companies.shift();
+console.log(companies)
+companies.splice(2,1,"Ola");
+//lets do functiojs now
+
+//fucntion definition
+function myFucntion(){
+    console.log("hello this is my function");
+}

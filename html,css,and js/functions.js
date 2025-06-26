427f5b48bbdba2b307893a11204bf19831b44624
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-26 23:37:28 +0530

    functions

--- html,css,and js/functions.js
+++ html,css,and js/functions.js
@@ -1,24 +1,40 @@
 //firsts lets do arrays
-let companies=["Bloomberg","Microsoft","Google", "Uber", "IBM", " Netflix"];
-console.log(companies)
+let companies = ["Bloomberg", "Microsoft", "Google", "Uber", "IBM", " Netflix"];
+console.log(companies);
 companies.shift();
-console.log(companies)
-companies.splice(2,1,"Ola");
+console.log(companies);
+companies.splice(2, 1, "Ola");
 //lets do functiojs now
 
 //fucntion definition
-function myFucntion(){
-    console.log("hello this is my function");
+function myFucntion() {
+  console.log("hello this is my function");
 }
 
-function sum(a, b){
-    console.log(a+b);//we can even just return our input here using return keyword..but return can only reurn one value
-    
+function sum(a, b) {
+  console.log(a + b); //we can even just return our input here using return keyword..but return can only reurn one value
 }
-sum(2,3);
+sum(2, 3);
 
 //arrow functions
-const arrowSum = (a,b)=>{
-    console.log(a+b);
-}
+const arrowSum = (a, b) => {
+  console.log(a + b);
+};
 
+//create a function using functon keyword that takes a string as an argument and return the number of vowels in the string
+function returnVowels(msg) {
+  let msg = prompt("Enter your string");
+  let count = 0;
+  for (const char of msg) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  console.log(count);
+}

fea8ba976d27a0a15e40fd53ebcfbfc5ec488ec6
AakritiA04 <aroraaakriti04@gmail.com>
2025-06-29 23:39:07 +0530

    functions

--- html,css,and js/functions.js
+++ html,css,and js/functions.js
@@ -54,3 +54,37 @@ let arrarytoSq=[1,4,66,78,894,2,33,44,5];
 arrarytoSq.forEach((value)=>{
   console.log(value*value);
 }); 
+
+let newArr= arrarytoSq.map((val)=>{
+  console.log(val);
+})
+
+//reduce method
+//filter method
+
+//now practice qyestion use filter to filter out marks of student that are 90+
+let studMarks=[22,87,98,77,99,65,95,94,21];
+
+let output=studMarks.filter((val)=>{
+  return val>=90 ;
+})
+
+//take a numver n as an input from the user . Create an array of numers from 1 to n.
+//use reduce method to calculate sum of all numbers in the array.
+//use reduce to calculate product of all numbers in a array
+
+let n=prompt("enter your number");
+let arr1 =[];
+for(let i=1;i<=n;i++){
+  arr1[i-1]=i;
+}
+console.log(arr1);
+
+let summ=arr1.reduce((prev, curr)=>{
+  return prev+curr;
+});
+
+//this is technically factorial of 1 to n numbers. 
+let product=arr1.reduce((res,curr)=>{
+  return res*curr;
+})

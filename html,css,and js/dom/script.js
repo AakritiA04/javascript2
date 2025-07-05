b655caf2137d9b594f38bba3cc49cde9ab29ebcd
AakritiA04 <aroraaakriti04@gmail.com>
2025-07-05 23:58:33 +0530

    dom

--- html,css,and js/dom/script.js
+++ html,css,and js/dom/script.js
@@ -1,3 +1,5 @@
 //when a web oafe is loaded the browser creates a documnet object model of page
-windows.document
-document.getElementById
+//there are three properties used
+//getelementbyID
+///getElementbyClassName
+//getElementByTagName

9e0262e1733ebb536367563ec4fca1c5246a35ce
AakritiA04 <aroraaakriti04@gmail.com>
2025-07-09 23:56:26 +0530

    new

---
+++ html,css,and js/dom/practice3.js
@@ -0,0 +1,10 @@
+//question
+//create a h2 element with text -"hello javascript" append "from apna collge students" to this using js
+
+//appedning is adding to the last of the snetnce
+let h2= document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText= h2.innerText + " from Apna college students"
+console.log(h2)
+//we are here concatinating

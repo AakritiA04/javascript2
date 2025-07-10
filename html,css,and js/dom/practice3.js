92ec073fc7ca181df2ac622ab231653b34efc8f9
AakritiA04 <aroraaakriti04@gmail.com>
2025-07-10 15:01:35 +0530

    new

--- html,css,and js/dom/practice3.js
+++ html,css,and js/dom/practice3.js
@@ -2,9 +2,32 @@
 //create a h2 element with text -"hello javascript" append "from apna collge students" to this using js
 
 //appedning is adding to the last of the snetnce
-let h2= document.querySelector("h2");
-console.dir(h2.innerText);
+// let h2= document.querySelector("h2");
+// console.dir(h2.innerText);
 
-h2.innerText= h2.innerText + " from Apna college students"
-console.log(h2)
+// h2.innerText= h2.innerText + " from Apna college students"
+// console.log(h2)
 //we are here concatinating
+
+//question2 
+//create 3 divs with common class name"box. Access them and some unique text to each of them
+
+// let divs=document.querySelectorAll(".box");
+// divs[0].innerText="new unique value 1";
+// divs[1].innerText="new uni value2";
+// //we can also be using loops to accesthem across just like how we did in arrays
+// let idx=0
+// for(div of divs){
+//     // console.log(div.innerText);
+//     div.innerText=`new unique value ${idx}`;
+//     console.log(div);
+//     idx++;
+// }
+
+let newButton= document.createElement("button");
+newButton.innerText="click me";
+console.log(newButton);
+//adding to the screen
+let div= document.querySelector("div");
+div.append(newButton);
+

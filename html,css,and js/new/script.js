d30f52e1a4172052ab50cdc72a179a6fa60d8160
AakritiA04 <aroraaakriti04@gmail.com>
2025-07-27 17:02:04 +0530

    classes

--- html,css,and js/new/script.js
+++ html,css,and js/new/script.js
@@ -1,9 +1,25 @@
-//direct way of writing an object
+// //direct way of writing an object
 
-const student={
-    fullName:"Aakrit Aroa",
-    marks:95,
-    printMarks: function(){
-        console.log("marks=",this.marks);
+// const student={
+//     fullName:"Aakrit Aroa",
+//     marks:95,
+//     printMarks: function(){
+//         console.log("marks=",this.marks);
+//     }
+// }
+
+//making our own prototype
+const employee={
+    calTax(){
+        console.log("tax rate is 10%");
     }
-}
+};
+
+const karanArjun={
+    salary:50000,
+    calTax(){
+        console.log("tax is 20%");
+    },
+};
+karanArjun.__proto__=employee;
+ 

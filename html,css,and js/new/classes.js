1919771ba783ff6eeaabd8770b0fed96f261f6a8
AakritiA04 <aroraaakriti04@gmail.com>
2025-07-27 17:05:29 +0530

    classes

---
+++ html,css,and js/new/classes.js
@@ -0,0 +1,12 @@
+class ToyotaCar{
+    //har car start ho skti hai and har car stop ho skti hai
+    start(){
+        console.log("start");
+    }
+
+    stop(){
+        console.log("stop");
+    }
+}
+
+let fortuner= new ToyotaCar(); 

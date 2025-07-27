9e84ae7bd4c39a20d477f130e556191a74db2415
AakritiA04 <aroraaakriti04@gmail.com>
2025-07-27 23:53:19 +0530

    classes

--- html,css,and js/new/classes.js
+++ html,css,and js/new/classes.js
@@ -7,6 +7,13 @@ class ToyotaCar{
     stop(){
         console.log("stop");
     }
+
+    setBrand(brand){
+     this.brandName= brand;
+
+    }
 }
 
 let fortuner= new ToyotaCar(); 
+let lexus= new ToyotaCar();
+   
